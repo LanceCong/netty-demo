67b48fb7fbac4952e6f06f8e5ac7b4e3016657b0
shicong.liang <809556311@qq.com>
2018-01-03 10:20:19 +0800

    netty in action demo add

---
+++ src/main/java/com/example/demo/tio/Const.java
@@ -0,0 +1,23 @@
+package com.example.demo.tio;
+
+/**
+ * Created by lance on 2018/1/2.
+ */
+public class Const {
+
+    /**
+     * 服务器地址
+     */
+    public static final String SERVER = "127.0.0.1";
+
+    /**
+     * 监听端口
+     */
+    public static final int PORT = 6789;
+
+    /**
+     * 心跳超时时间
+     */
+    public static final int TIMEOUT = 50000;
+
+}
